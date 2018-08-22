9f56c21f5701a183eabb9d8d5c39d2096bf30aeb
EC2 Default User <ec2-user@ip-172-31-10-97.us-west-2.compute.internal>
2018-08-22 22:55:45 +0000

    use OpenCV to crop incoming image

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import numpy as np
 import os
 import importlib
 from model import Model
-import tensorflow as tf 
+# import matplotlib.pyplot as plt
+import tensorflow as tf    
+import cv2     
+
+
 
 
 
@@ -18,15 +22,19 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
+face_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
 model = Model()
 graph = tf.get_default_graph()
 
 def img_preparation(img):
+
+    cv2.imwrite('tmp1.jpg',img)
+    img = cv2.resize(img,(350,350))
+    cv2.imwrite('tmp2.jpg',img)
+    img = img / 255.0
     
-    
-    img_array = img_to_array(img)
-    img_array = img_array / 255.0
-    img_array = img_array.reshape(1,350,350,3)
+    img_array = img.reshape(1,350,350,3)
     return img_array
 
 
@@ -38,12 +46,38 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    bounding_offset = 15
     if request.method == 'POST':
         print("predict handler invoked")
         f = request.files['img_file']
         f.save('./query_img.jpg')
 
-        img = load_img("./query_img.jpg",target_size=(350, 350))
+        img = cv2.imread('./query_img.jpg')
+        
+
+       
+
+        # img = load_img("./query_img.jpg",target_size=(350, 350))
+
+        # img = img_to_array(img)
+        
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
+        if faces is ():
+            pass 
+        else:
+            for (x,y,w,h) in faces:
+                x_start = y - bounding_offset  if y - bounding_offset >= 0  else y
+                x_end = y+h+bounding_offset  if y+h+bounding_offset <= img.shape[0]  else y+h
+                y_start =  x - bounding_offset if x - bounding_offset >= 0 else x
+                y_end = x+w+bounding_offset  if x+w+bounding_offset <= img.shape[1] else x+w
+                
+                img = img[x_start:x_end, y_start:y_end, :]    
+
+
         img_array = img_preparation(img)
 
         global graph
